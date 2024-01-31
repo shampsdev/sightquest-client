9ac6ddd471d9433f19303fc63cba478d5cb9bbd3
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-01-31 19:31:54 +0300

    Added ENV variables

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,15 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      "nativewind/babel",
-      "react-native-reanimated/plugin",
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
     ],
   };
 };
